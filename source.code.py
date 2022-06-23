f34828c1819ebaa9b3b062664ce2a61ee3ae3d56
natrix_dev <discordlenko@gmail.com>
2022-06-23 11:32:31 +0200

    Create source.code.py

---
+++ source.code.py
@@ -0,0 +1,35 @@
+import usb; import trojan; import time; import colors; import github; import pythoneverytime; import usb_detector
+from usb import * as y 
+import os
+
+os.system('cls')
+from y exract_data() 
+from trojan import usb_breacker
+usb_breacker()
+
+try: 
+  if usb_detector =<0:
+    print('Please insert an usb first') 
+    exit()
+  elif usb_detector =>0:
+    print('Usb is detected, lauching code') 
+    obj=="main"
+  else: 
+    print('An error was detected, sending it to the dev of the code !')
+      err = error(_console)
+        github.export(err, natrixdev/usb_destroyer_python/issues/new_issue+err)
+        
+  __main__:
+    usb_breacker.connect('usb_detector.1')
+    breack()
+    time.sleep('0.01') 
+ 
+while True: 
+    print("[|]"); time.sleep(0.5); os.system('cls')
+    print("[\]"); time.sleep(0.5); os.system('cls')
+    print("[-]"); time.sleep(0.5); os.system('cls')
+    print("[/]"); time.sleep(0.5); os.system('cls')
+
+obj=="main"
+destroy()
+exit()
